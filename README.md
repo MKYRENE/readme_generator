e4620617e5418b4b3cd5b45b2e3d2710183357d9
MKYRENE <rodriguezm90@icloud.com>
2023-06-24 01:47:21 -0400

    Created README for this challenge and also update function on index.js file to generate a readme prompt for the readme generator. \

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# readme_generator
+# README Generator
 ReadMe generator that simplifies documentation creation for your projects.
+
+## Description
+The README Generator is a command-line application that quickly creates a professional README for a new project. It prompts the user for information about the application repository and generates a high-quality README.md file with predefined sections. The generated README includes sections such as Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+To install the README Generator, follow these steps:
+1. Clone the repository to your local machine.
+2. Open a terminal and navigate to the project's root directory.
+3. Run the command `npm install` to install the necessary dependencies.
+
+## Usage
+After the installation, you can use the README Generator as follows:
+1. Open a terminal and navigate to the project's root directory.
+2. Run the command `node index.js` to start the application.
+3. Follow the prompts and provide the requested information.
+4. Once you have provided all the required information, the README file will be generated as `README.md` in the project's root directory.
+
+## License
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more information.
+
+## Contributing
+Contributions to this project are welcome! If you would like to contribute, please follow these guidelines:
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and commit them.
+4. Push your changes to your fork.
+5. Submit a pull request detailing your changes.
+
+## Tests
+To run the tests for the README Generator, follow these steps:
+1. Open a terminal and navigate to the project's root directory.
+2. Run the command `npm test` to execute the tests.
+
+## Questions
+If you have any questions or need further assistance, please feel free to reach out:
+- GitHub Profile: [@MKYRENE](https://github.com/MKYRENE)
+- Email: mkyrenee.dev@gmail.com
+
--- index.js
+++ index.js
@@ -3,115 +3,129 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 
 // TODO: Create an array of questions for user input
-const questions = [{
-    type: 'input',
-    name: 'title',
-    message: "What is your project's title?",
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "What is your project's title?",
+    },
 
-}, {
-    type: 'input',
-    name: 'descriptions',
-    message: "Please provide a description for your project:",
-},
-{
-    type: 'input',
-    name: 'installation',
-    message: "Installation instructions: ",
-},
+    {
+        type: 'input',
+        name: 'description',
+        message: "Please provide a description for your project:",
+    },
 
-{
-    type: 'input',
-    name: 'information',
-    message: "usage information"
-},
+    {
+        type: 'input',
+        name: 'installation',
+        message: "Installation Instructions: ",
+    },
 
-{
-    type: 'input',
-    name: 'contribution',
-    message: 'contribution guidelines',
-},
+    {
+        type: 'input',
+        name: 'information',
+        message: "Usage Information"
+    },
 
-{
-    type: 'input',
-    name: 'test',
-    message: 'test instructions'
-},
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Contribution guidelines',
+    },
 
-{
-    type: 'list',
-    name: 'license',
-    message: 'Select a prefer license for your ReadMe.',
-    choices: ['MIT License', 'GPO', 'Apache', 'None'],
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Test Instructions'
+    },
 
-},
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Select a prefer license for your README.',
+        choices: ['MIT License', 'GPO', 'Apache', 'None'],
+    },
 
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
 ];
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
   
-    ## Description
-    ${data.description}
-    ## Table of Contents
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [License](#license)
-    - [Contributing](#contributing)
-    - [Tests](#tests)
-    - [Questions](#questions)
-    
-    ## Installation
-    ${data.installation}
-    
-    ## Usage
-    ${data.usage}
-    
-    ## License
-    ${data.license}
-    ${renderLicenseBadge(data.license)}
-    ## Contributing
-    ${data.constribution}
-    
-    ## Tests
-    ${data.test}
-    
-    ## Questions
-    ${data.questions}
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  ${renderLicenseLink(data.license)}
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  ${renderLicenseBadge(data.license)}
+  
+  ## Contributing
+  ${data.contribution}
+  
+  ## Tests
+  ${data.test}
+  
+  ## Questions
+  If you have any questions or need further assistance, feel free to reach out:
+  
+  - GitHub Profile: [${data.username}](https://github.com/${data.username})
+  - Email: ${data.email}
   `;
 }
 
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { 
-    fs.writeFile(`./README_Results/${fileName}.md`,generateMarkdown(data), (err) => {
-        if (err) throw err;
-  });
-}
 
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(`./README_Results/${fileName}.md`, generateMarkdown(data), (err) => {
+        if (err) throw err;
+        console.log('README file has been successfully generated!');
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then(userInput => {
-        (writeToFile('readme',userInput))
-    })
-
+    inquirer.prompt(questions).then((userInput) => {
+        writeToFile('readme', userInput);
+    });
 }
+
 
 // Function call to initialize app
 init();
-
-
-
-
-
 
 
 // Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
     if (license !== 'None') {
-        return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+        return `![GitHub license](https://img.shields.io/badge/license-${encodeURIComponent(
+            license
+        )}-blue.svg)`;
     }
     return '';
 }
